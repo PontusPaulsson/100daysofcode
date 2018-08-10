e475f829259ede1a33746fb8ac7508ad980f47ea
Pontus Paulsson <pontus.p.paulsson@gmail.com>
2018-08-10 09:23:11 +0200

    Added main.cs under src

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.*;
+import javafx.event.*;
+import javafx.scene.control.*;
+
+public class Main extends Application  {
+
+    Stage window;
+    Button btn1;
     public static void main(String[] args){
-        System.out.println("Hej");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("Stupid Title!");
+
+        btn1 = new Button("Click me!");
+
+        StackPane layout1 = new StackPane();
+        layout1.getChildren().add(btn1);
+
+        Scene scene1 = new Scene(layout1, 300,300);
+        window.setScene(scene1);
+        window.show();
+
+    }
+
 }
